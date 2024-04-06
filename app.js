24d05230e8d64b3b9b486d70d122a453a931dbe1
michelleevt <michelletanod78@gmail.com>
2024-04-07 00:39:07 +0800

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (req, res) => {
-    res.send('Michelle.v.t <br>, 51021005 <br>, Sistem Jurusan')
+    res.send('Michelle.v.t <br>, 51021005 <br>, Sistem Informasi')
 })
 
 app.listen(PORT, HOST)
